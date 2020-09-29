30f3d208c5a69354681ea482e34995a664dcd4d7
devaljain1998 <devaljain1998@gmail.com>
2020-09-29 13:37:35 +0530

    Commented other line segmenting methods and working on new one

--- Algorithms/LineBWRectangleAlgo/line_bw_rectangles.py
+++ Algorithms/LineBWRectangleAlgo/line_bw_rectangles.py
@@ -6,7 +6,7 @@ import math
 from pillarplus import math as ppmath
 
 file_path = 'Algorithms/LineBWRectangleAlgo/input/DXF/'
-input_file = 'sample1.dxf'
+input_file = 'sample6.dxf'
 output_file_path = 'Algorithms/LineBWRectangleAlgo/output/'
 input_file_name = input_file.split('.')[0]
 output_file = f'{input_file_name}_output.dxf'
@@ -158,7 +158,8 @@ def is_closed(polyline):
 
 #Convert polylines into lines:
 print('Converting polylines into lines.')
-lines = [] 
+lines = []
+line_meta = {}
 for polyline in polylines:
     line = []
     
@@ -175,6 +176,8 @@ for polyline in polylines:
             
             #Append the line into lines
             lines.append(line)
+            line_meta[str(line)] = {'polyline' : polyline}
+            
             #clear the line
             line = [(x, y)]
     
@@ -253,6 +256,8 @@ def get_distance_of_all():
             
 # get_distance_of_all()
 
+def does_lines_belong_to_same_polyline(line1, line2) -> bool:
+    return line_meta[str(line1)]['polyline'] == line_meta[str(line2)]['polyline']
 
 # loop through lines of each slope and draw a line in the centre
 parallel_line_pairs = []
@@ -297,7 +302,10 @@ for slope, lines in slopes.items():
                 line2 = _get_line2(index, counter)
                 continue                
                 
-            
+            if does_lines_belong_to_same_polyline(line1, line2):
+                counter += 1
+                line2 = _get_line2(index, counter)
+                continue                                
             
             print(f'Forming a pair b/w {line1, line2}, slope: {slope}\n')
             #Make pair of these lines
@@ -341,6 +349,35 @@ def get_direction(line_to_be_moved, other_line):
     
     return dir
 
+def get_gfg_direction(line_to_be_moved, other_line):
+    # fetch the highest point 
+    max_point = max(line_to_be_moved[0], line_to_be_moved[1])
+    
+    directions = {1 : "RIGHT", -1 : "LEFT", 0 : "ON THE LINE"}
+    from ezdxf.math import Vec2
+    A = Vec2(other_line[0])
+    B = Vec2(other_line[1])
+    P = Vec2(max(line_to_be_moved[0], line_to_be_moved[1]))
+    
+    B.x -= A.x; 
+    B.y -= A.y; 
+    P.x -= A.x; 
+    P.y -= A.y; 
+    
+    cross_product = B.x * P.y - B.y * P.x
+    
+    RIGHT, LEFT, ZERO = 1, -1, 0
+    
+    if (cross_product > 0):
+        return RIGHT; 
+  
+    #return LEFT if cross product is negative 
+    if (cross_product < 0):
+        return LEFT; 
+  
+    #return ZERO if cross product is zero.  
+    return ZERO;    
+
 def get_non_segmented_lines(line1, line2):
     #Start center points:
     x1, y1, x2, y2 = get_centre_points(line1, line2)
@@ -365,6 +402,8 @@ def get_non_segmented_lines(line1, line2):
     msp.add_line((x1, y1), (x2, y2), dxfattribs={'layer': 'CenterLines'})
     
     
+debug_parallel_line_pairs = []    
+
 def get_segmented_line(line1, line2):
     """This function returns a line segment in between the two the parallel line pairs.
     Logic has been taken from MathStackExchange (link given below).
@@ -392,10 +431,12 @@ def get_segmented_line(line1, line2):
         r = line1_length
         print(f'LinetobeMoved: {line_to_be_moved}, r: {r}')
         direction = get_direction(line_to_be_moved, line2)
+        debug_parallel_line_pairs.append({'lines' : (line1, line2), 'DIR' : direction})
     else:
         line_to_be_moved = line2
         r = line2_length
         direction = get_direction(line_to_be_moved, line1)
+        debug_parallel_line_pairs.append({'lines' : (line1, line2), 'DIR' : direction})
         
     # Make a new line which is at half the distance between line1 and line2
     distance = ppmath.get_distance_between_two_parallel_lines(line1, line2)
@@ -417,20 +458,91 @@ def get_segmented_line(line1, line2):
     
     return [(x3, y3), (x4, y4)]
 
+def get_centered_line_segments(line1, line2) -> list:
+    """Function which returns a centered line segments from parallel pairs.
+
+    Args:
+        line1 (List of Tuples): A line is a collection of tuple of points in the following manner [(x1, y1), (x2, y2)].
+        line2 (List of Tuples): A line is a collection of tuple of points in the following manner [(x1, y1), (x2, y2)].
+        
+    Returns:
+        line (List of Tuples): A linesegment in middle of line1 and line2.
+    """
+    # Calculate lenght of both the line segment.
+    line1_length = ppmath.get_length_of_line_segment(line1)
+    line2_length = ppmath.get_length_of_line_segment(line2)
+    
+    smaller_line = line1 if line1_length <= line2_length else line2
+    bigger_line = line1 if smaller_line == line2 else line1
+    
+    # We need to get perpendicular points from the smaller line to the bigger line
+    p1 = smaller_line[0]
+    perpendicular_point1 = ppmath.find_perpendicular_point(p1, bigger_line[0], bigger_line[1])
+    
+    # Check if the perpendicular point exists on the other line or not:
+    if ppmath.is_between(perpendicular_point1, bigger_line[0], bigger_line[1]):
+        # Find centre point in this case
+        centre_point1 = ppmath.get_mid_points_between_points(p1, perpendicular_point1)
+    else:
+        # we need to get the point from the bigger line now
+        for point in bigger_line:
+            perpendicular_point1 = ppmath.find_perpendicular_point(point, smaller_line[0], smaller_line[1])
+            # if this perpendicular point lies in the smaller line then break
+            if ppmath.is_between(perpendicular_point1, smaller_line[0], smaller_line[1]):
+                # calculate the centre point first
+                centre_point1 = ppmath.get_mid_points_between_points(point, perpendicular_point1)                
+                break
+                
+    # We need to get perpendicular points from the smaller line to the bigger line
+    p2 = smaller_line[0]
+    perpendicular_point2 = ppmath.find_perpendicular_point(p2, bigger_line[0], bigger_line[1])
+    
+    # Check if the perpendicular point exists on the other line or not:
+    if ppmath.is_between(perpendicular_point1, bigger_line[0], bigger_line[1]):
+        # Find centre point in this case
+        centre_point2 = ppmath.get_mid_points_between_points(p2, perpendicular_point2)
+    else:
+        # we need to get the point from the bigger line now
+        for point in bigger_line:
+            perpendicular_point2 = ppmath.find_perpendicular_point(point, smaller_line[0], smaller_line[1])
+            # if this perpendicular point lies in the smaller line then break
+            if ppmath.is_between(perpendicular_point2, smaller_line[0], smaller_line[1]):
+                # calculate the centre point first
+                centre_point2 = ppmath.get_mid_points_between_points(point, perpendicular_point1)
+                break
+            
+    return [centre_point1, centre_point2]
+    
+    
 for pair in parallel_line_pairs:
     line1, line2 = pair
     
     if line2:
+        x1, y1, x2, y2 = ppmath.get_line_points_2d(line1)
+        
+        if slope != math.inf:
+            try:
+                slope = round(get_slope(x1, y1, x2, y2))
+            except OverflowError as oe:
+                print(oe)
+                print('slope', slope)
+            
         print(f'Now calculating line segment {line1, line2}')
-        # get_non_segmented_lines(line1, line2)
-        line_segment = get_segmented_line(line1, line2)
-        print(f"""
-              line1: {line1},
-              line2: {line2},
-              line_segment: {line_segment}\n\n
-              """)
+        
+        # if slope == 0 or slope == math.inf:
+        #     line_segment = get_non_segmented_lines(line1, line2)
+        # else:
+        #     line_segment = get_segmented_line(line1, line2)
+            
+        # print(f"""
+        #       line1: {line1},
+        #       line2: {line2},
+        #       line_segment: {line_segment}\n\n
+        #       """)
         msp.add_line(line_segment[0], line_segment[1], dxfattribs={'layer': 'CenterLines'})
         
+print('Debug_parallel_line_pairs')
+pprint.pprint(debug_parallel_line_pairs)
         
 print('Success now saving the file')
 #Saving the final file:
