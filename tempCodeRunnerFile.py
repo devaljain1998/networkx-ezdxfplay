afda255be06180dd8c7f058d4985aae010c84b8e
devaljain1998 <devaljain1998@gmail.com>
2020-12-16 14:37:04 +0530

    Components are coming fine

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,1 @@
-conversion_factor = {
-    'mm': 1.0,
-    'inch': 0.0393701
-}
-centre_lines = get_centre_lines(
-    msp, dwg, "", conversion_factor=conversion_factor['inch'],
-    lines = cleaned_wall_lines)
-pprint([centre_line.__dict__ for centre_line in centre_lines])
+char_height
